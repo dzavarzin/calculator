82b3ddd9290b2455a98dc57eabb1a97962755a7f
Paolo Zanotti <zanotti.paolo2001@gmail.com>
2020-10-02 02:01:42 +0200

    Add KeyCode press, fix Mul and Div problem, add CLS and equals buttons, change long to double and move the main class in a folder called Model.

---
+++ Model/Calc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Model
+{
+    public class Calc
+    {
+        private static double currentResult = 0;
+
+        public static double GetResult()
+        {
+            return currentResult;
+        }
+
+        public static void Add(double n1, double n2)
+        {
+            currentResult = n1 + n2;
+        }
+
+        public static void Substract(double n1, double n2)
+        {
+            currentResult = n1 - n2;
+        }
+
+        public static void Multiply(double n1, double n2)
+        {
+            currentResult = n1 * n2;
+        }
+
+        public static void Divide(double n1, double n2)
+        {
+            currentResult = n1 / n2;
+        }
+        
+        public static void Clear()
+        {
+            currentResult = 0;
+        }
+    }
+}
